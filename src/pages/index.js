edd633663299ca62ddb9996763468c067dde4d00
Randal Vance Cunanan <randal@randalvance.net>
2020-02-29 22:49:13 +0800

    Change site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,17 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Test Site"
+    const siteTitle = "Randal Vance Cunanan"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          keywords={[`randal`, `cunanan`, `blog`, `javascript`, `react`]}
         />
-        <Link to="/blog/">
+        {/* <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
-        </Link>
+        </Link> */}
       </Layout>
     )
   }
